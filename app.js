baa86fc406d569f74bc976312b3779cd84493d23
Emily-allende <79849579+Emily-allende@users.noreply.github.com>
2022-07-30 21:00:29 -0300

    Update app.js

--- app.js
+++ app.js
@@ -15,15 +15,55 @@ const listName = [
     telefono: "12345678",
     id: 2,
   },
+  {
+    nombre: "Camila",
+    telefono: "09007523",
+    id: 3,
+  },
 ];
 
 app.get("/contactos", (request, response, next) => {
   response.json(listName);
 });
 
-app.post("/contactos", () => {
-  
+app.post("/contactos", (request, response, next) => {
+  const id = listName.length + 1;
+  const { nombre, telefono } =request.body;
+  const newContacto= { ...request.body, id};
+  if(nombre && telefono && id){
+    listName.push(newContacto);
+    response.json(listName);
+  } else {
+    response.status(500).json({error:'There was an error'})
+  }
 });
 
+app.put("/contactos/:id",(request, response, next) => {
+  const { id } = request.params;
+  const { nombre, telefono } =request.body;
+  if(nombre && telefono && id) {
+    _.each(listName, (listName, i) => {
+      if(listName.id === id) {
+        listName.nombere= nombre;
+        listName.telefono= telefono;
+      }
+    });
+    response.json(listName);
+  } else {
+    response.status(500).json({error:'There was an error'})
+  }
+});
+
+app.delete('/contactos/:id', (request, response, next)=> {
+  const { id } = request.params;
+  if(id) {
+    _.each(listName, (listName, i) => {
+      if(listName.id === id) {
+        listName.splice(i, 1);
+      }
+    });
+    response.json(listName);
+  }
+})
 app.listen(4000, () => console.log("Servidor corriendo en el puerto 4000"));
 console.log("Yendo a buscar los datos");
